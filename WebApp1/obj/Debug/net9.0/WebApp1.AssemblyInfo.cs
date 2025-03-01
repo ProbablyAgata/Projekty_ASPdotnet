1b81bf157d082f3d611cee5cec02de05641a824f
Agata Ochocińska <agata.ochocinska@wp.pl>
2025-03-01 12:08:50 +0100

    zmiany

--- WebApp1/obj/Debug/net9.0/WebApp1.AssemblyInfo.cs
+++ WebApp1/obj/Debug/net9.0/WebApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fdfdc564f5099596c78cb4004cbad2cc8f4f5ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f8da396a5f898a09245665fdc37b97242f615d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
